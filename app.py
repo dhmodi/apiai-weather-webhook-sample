ab70150017f4c262e24ce73b2e728b449004957d
US\dhmodi <dhmodi@deloitte.com>
2017-05-30 13:10:21 +0530

    d

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ def processRequest(req):
         data = json.loads(result)
         res = makeWebhookWeatherResult(data)
         return res
-    if req.get("result").get("action") == "weather.temperature":
+
+    elif req.get("result").get("action") == "weather.temperature":
         baseurl = "https://query.yahooapis.com/v1/public/yql?"
         yql_query = makeYqlQuery(req)
         if yql_query is None:
@@ -69,9 +70,11 @@ def processRequest(req):
         data = json.loads(result)
         res = makeWebhookTemperatureResult(data)
         return res
-    if req.get("result").get("action") == "identify.disease":
+
+    elif req.get("result").get("action") == "identify.disease":
         baseurl = "https://sandbox-healthservice.priaid.ch/diagnosis?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFsYWwuYWxtbGxAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxNzE3IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE3LTA1LTMwIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE0OTYxMzA0MDYsIm5iZiI6MTQ5NjEyMzIwNn0.hejsa1UK29gubSMuTUCCaZwlgJtCguskYowMByDJJ5E&gender=male&language=en-gb&year_of_birth=1988&"
         yql_query = makeSymptomsQuery(req)
+        print(yql_query)
         if yql_query is None:
             return {}
         yql_url = baseurl + urlencode(yql_query) + "&format=json"
@@ -80,6 +83,7 @@ def processRequest(req):
         data = json.loads(result)
         res = makeWebhookDiagnosisResult(data)
         return res
+
     else:
         return {}
 
@@ -105,7 +109,7 @@ def makeSymptomsQuery(req):
     print(symptoms)
     if symptoms is None:
         return None
-    
+
     return "symptoms=[" + symptoms + "]"
 
 
