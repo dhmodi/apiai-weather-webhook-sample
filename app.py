dd0b709f1ba1a86920481cedf24c4e0e84a423d4
US\dhmodi <dhmodi@deloitte.com>
2017-05-30 16:34:18 +0530

    ff

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def processRequest(req):
         print(yql_query)
         if yql_query is None:
             return {}
-        yql_url = baseurl + urlencode(yql_query)
+        yql_url = baseurl + yql_query
         print(yql_url)
         result = urlopen(yql_url).read()
         print(json.dumps(result))
