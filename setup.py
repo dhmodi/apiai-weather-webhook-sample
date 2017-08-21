fca3b25e4ed2fba67cf3b111d5ddcb49f47bb150
dhmodi <dhavalmodi24@gmail.com>
2017-08-21 17:42:00 +0530

    database integration

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name = "virtual-patient",
+      package_dir = {''},
+      packages = ["cognitiveSQL"],
+      scripts = ["app.py"],
+      )
